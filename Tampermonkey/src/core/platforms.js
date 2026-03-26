72da12d15eabc8d8861e54a52698f81d98f27856
polprats <64233829+polprats@users.noreply.github.com>
2026-03-26 22:09:50 +0100

    Add platform examples for campus and IOC

--- Tampermonkey/src/core/platforms.js
+++ Tampermonkey/src/core/platforms.js
@@ -1,1 +1,22 @@
-// platforms.js
+(function () {
+    'use strict';
+
+    window.TM = window.TM || {};
+
+    window.TM.Platforms = {
+        campusExample: {
+            id: 'campus-example',
+            label: 'Campus Example',
+            matchesHost() {
+                return window.location.hostname === 'example-campus.invalid';
+            }
+        },
+        iocExample: {
+            id: 'ioc-example',
+            label: 'IOC Example',
+            matchesHost() {
+                return window.location.hostname === 'example-ioc.invalid';
+            }
+        }
+    };
+})();
