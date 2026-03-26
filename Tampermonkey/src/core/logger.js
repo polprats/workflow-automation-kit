d5e756b10d8c49db1e1c5bbff0a0993d6eeda9aa
polprats <64233829+polprats@users.noreply.github.com>
2026-03-26 22:08:56 +0100

    Implement TM.Logger for logging messages

--- Tampermonkey/src/core/logger.js
+++ Tampermonkey/src/core/logger.js
@@ -1,1 +1,19 @@
-// logger.js
+(function () {
+    'use strict';
+
+    window.TM = window.TM || {};
+
+    const LOG_PREFIX = '[TM Modular]';
+
+    window.TM.Logger = {
+        log(...args) {
+            console.log(LOG_PREFIX, ...args);
+        },
+        warn(...args) {
+            console.warn(LOG_PREFIX, ...args);
+        },
+        error(...args) {
+            console.error(LOG_PREFIX, ...args);
+        }
+    };
+})();
